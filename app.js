ffe32dff1a26261b48afc72dcc9b74687db4dd1f
Bhavesh Bhimte <jammerking96@gmail.com>
2021-01-04 17:42:20 +0530

    fixed the main UI Layout and logic for computer choice

---
+++ app.js
@@ -0,0 +1,21 @@
+//Declare Global Variables
+
+const rockSelector = document.querySelector("#rock");
+const paperSelector = document.querySelector("#paper");
+const scissorsSelector = document.querySelector("#scissors");
+const computerChoiceArray = ["rock", "paper", "scissors"];
+
+//Declaring global eventlistener
+
+rockSelector.addEventListener("click", runLog);
+paperSelector.addEventListener("click", runLog);
+scissorsSelector.addEventListener("click", runLog);
+
+// Declaring global Functions
+
+function runLog() {
+  for (let i = 0; i < computerChoiceArray.length; i++) {
+    console.log(computerChoiceArray[[Math.floor(Math.random() * 3)]]);
+    break;
+  }
+}
